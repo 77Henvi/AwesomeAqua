4f4fd631e65c6b6ccf25f58fc7a57e286a4d9a10
Fifa <132676120+77Henvi@users.noreply.github.com>
2026-06-25 19:52:05 +0700

    coming home [F1lthy]
    -- johndoe with the push

--- scripts/adminState.js
+++
@@ -1,9 +0,0 @@
-// ไฟล์ scripts/adminState.js
-export const state = {
-  fishData: [],
-  financeData: [],
-  profitMap: {},
-  stockFilter: 'all',
-  finFilter: 'all',
-  selectedFinMonth: new Date().toLocaleDateString('en-CA').slice(0, 7)
-};
